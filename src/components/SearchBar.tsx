d668cb2c431a69338137248084cd498a9ce307ec
oyemiwisdom <oyemiwisdom@gmail.com>
2026-07-07 00:37:47 -0400

    Created searchbar element.

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+import TextField from '@mui/material/TextField'
+
+function SearchBar() {
+  return (
+    <Box
+      component="form"
+      sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
+      noValidate
+      autoComplete="off">
+        <TextField id="outlined-basic" label="What we feel for today???" variant="outlined" />
+      
+    </Box>
+  )
+}
+
+export default SearchBar
