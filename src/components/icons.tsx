23034aa2cc92d0a8f1c4814753137b7978d143c9
oyemiwisdom <oyemiwisdom@gmail.com>
2026-07-08 21:25:56 -0400

    Add icon components for use

---
+++ src/components/icons.tsx
@@ -0,0 +1,10 @@
+import SettingsIcon from '@mui/icons-material/Settings'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'
+
+function icons() {
+  return (
+    <div>icons</div>
+  )
+}
+
+export default icons
