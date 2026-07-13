75f34cf74281b6c6f2f9d104fd1bc63014854ad9
Jovan Clue <jovan.clue@bison.howard.edu>
2026-07-12 22:13:21 -0500

    Add necessary elements for styling

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,15 +1,29 @@
 import SearchBar from "@components/SearchBar.tsx";
 import AccountBtn from "@components/AccountBtn/AccountBtn.tsx";
+import SettingsBtn from "@components/SettingsBtn/SettingsBtn.tsx";
 import '@stylesheets/App.css'
 import { Link } from "react-router";
 
 export default function Home(){
     return <>
         <div id="intro-txt">Let's find some eats!</div>
+
         <SearchBar></SearchBar>
+
         <div id="return-bar">Returned stuff go here</div> {/* Temporary text to show where returned stuff would go*/}     
-        <Link to="/account"><AccountBtn></AccountBtn></Link>
-        <Link to="/settings">Settings (Placeholder)</Link>
+
+        <div id="nav-tools">
+            <div className="nav-items">
+                <Link to="/account"><AccountBtn></AccountBtn></Link>
+                <p>User <br/> Account</p>
+            </div>
+
+            <div className="nav-items">
+                <Link to="/settings"><SettingsBtn></SettingsBtn></Link>
+                <p>Settings</p>
+            </div>
+        </div>
+
     </>;
     
 }
