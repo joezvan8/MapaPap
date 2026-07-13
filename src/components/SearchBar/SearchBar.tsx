c647c2c045a03612800c0eb7bd118365a50ce318
oyemiwisdom <oyemiwisdom@gmail.com>
2026-07-13 12:26:08 -0400

    Add style to search bar.

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,13 +1,14 @@
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import styles from './SearchBar.module.css';
 
 function SearchBar() {
   return (
     <Box
-      component="form"
-      sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
-      noValidate
-      autoComplete="off">
+        className={styles.searchBar}
+        component="form"
+        noValidate
+        autoComplete="off">
         <TextField id="outlined-basic" label="What we feel for today???" variant="outlined" />
       
     </Box>
