8da3d4b877465a12b93a5c175457e40e5a65b6ef
Jovan Clue <jovan.clue@bison.howard.edu>
2026-07-15 15:52:47 -0500

    Make return bar into component and add styles

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,21 +1,22 @@
 import  SearchBar from "@components/SearchBar/SearchBar.tsx";
 import AccountBtn from "@components/AccountBtn/AccountBtn.tsx";
 import SettingsBtn from "@components/SettingsBtn/SettingsBtn.tsx";
+import ReturnBar from "@components/ReturnBar/ReturnBar.tsx";
 import '@stylesheets/App.css'
 import { Link } from "react-router";
-import useFetch from '../hooks/useFetch.js'
 
 
-const searchURL = new URL('https://nominatim.openstreetmap.org/search?q=bakery+in+may+pen+jamaica&format=jsonv2&limit=5')
+
+
 
 export default function Home(){
-    const [data] = useFetch(searchURL);
+
     return<>
         <div id="intro-txt">Let's find some eats!</div>
 
         <div id="search-bar-thingy"><SearchBar></SearchBar></div>
 
-        <div id="return-bar">Returned stuff go here {JSON.stringify(data)}</div> {/* Temporary text to show where returned stuff would go*/}
+        <div id="return-bar"><ReturnBar></ReturnBar></div> {/* Temporary text to show where returned stuff would go*/}
 
         <div id="nav-tools">
             <div className="nav-items">
