a986a7422e4b1a1d8d31d43a479bc7f9f11f1b9a
Jovan Clue <jovan.clue@bison.howard.edu>
2026-07-15 15:52:12 -0500

    Fix aliases

--- vite.config.ts
+++ vite.config.ts
@@ -11,9 +11,9 @@ export default defineConfig({
       "@assets": path.resolve(__dirname, "./src/assets"), 
       "@pages": path.resolve(__dirname, "./src/pages"),
       "@stylesheets": path.resolve(__dirname, "./src/stylesheets"),
-      "@utils": path.resolve(__dirname, "src/utils/*"),
-      "@hooks": path.resolve(__dirname, "src/hooks/*"),
-      "@features": path.resolve(__dirname, "src/features/*"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@features": path.resolve(__dirname, "./src/features"),
     }
   }
 })
