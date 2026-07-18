93df4d114572424ed462f347ea5c484f4c18d285
Jovan Clue <jovan.clue@bison.howard.edu>
2026-07-17 22:34:21 -0500

    Add contextx alias to config files

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
       "@utils": path.resolve(__dirname, "./src/utils"),
       "@hooks": path.resolve(__dirname, "./src/hooks"),
       "@features": path.resolve(__dirname, "./src/features"),
+      "@contexts": path.resolve(__dirname, "./src/contexts"),
     }
   }
 })
