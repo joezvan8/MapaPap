97930e641263f1849d536d6d66b7d3298ac506b9
Jovan Clue <jovan.clue@bison.howard.edu>
2026-07-20 14:16:07 -0500

    Add loading state to custom fetch hook

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,27 +2,35 @@ import { useState, useEffect } from "react";
 
 
 const useFetch = (url) => {
+    const [loading, setLoading] = useState(false)
     const [data, setData] = useState();
 
     useEffect(() => {
             const fetchData = async () => {
+                setLoading(true)
                 try {
+
                     const response = await fetch(url);
                     if (!response.ok) {
                         throw new Error(`Response status: ${response.status}`)
                     }
 
                     const result = await response.json();
-                    setData(result);
+                    setTimeout(() => {
+                        setData(result);
+                        setLoading(false)
+                    }, 1000)
 
                 } catch (error) {
                     console.error(error)
+                    setLoading(false)
                 }
+
             };
         void fetchData();
         }, [url]
     )
-    return [data];
+    return [data, loading];
 };
 
 export default useFetch;
