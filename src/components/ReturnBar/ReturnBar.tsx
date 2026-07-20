594086f8754346236e1e260fc5a189a4a60d6a8e
Jovan Clue <jovan.clue@bison.howard.edu>
2026-07-20 14:17:11 -0500

    Add circular progress bar when loading

--- src/components/ReturnBar/ReturnBar.tsx
+++ src/components/ReturnBar/ReturnBar.tsx
@@ -2,14 +2,32 @@ import useFetch from '@hooks/useFetch';
 import styles from './ReturnBar.module.css';
 import {useContext} from "react";
 import {UrlContext} from "@utils/builder.ts";
+import {CircularProgress} from "@mui/material";
 
 
 export default function ReturnBar() {
     const { url } = useContext(UrlContext)
 
-    const [data] = useFetch(url) ?? ""
+    const [data, loading] = useFetch(url)
 
     return (
-        <div className={styles.returnBar}>{[JSON.stringify(data), url]}</div>
+        <div className={styles.returnBar}>
+                {
+                    loading ?
+                        <div className={styles.returnLoader}> <Loading /></div>   :
+                        [JSON.stringify(data), url]
+                }
+
+        </div>
     );
+}
+
+function Loading() {
+    return <CircularProgress
+        size="150px"
+        color="inherit"
+
+        aria-label="Loading..."
+    />
+
 }
