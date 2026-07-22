51f1bbc1b8770bd130e5123177943bf3c121e25f
Jovan Clue <jovan.clue@bison.howard.edu>
2026-07-22 11:19:58 -0500

    Update return bar component to only show display names of places

--- src/components/ReturnBar/ReturnBar.tsx
+++ src/components/ReturnBar/ReturnBar.tsx
@@ -3,24 +3,23 @@ import styles from './ReturnBar.module.css';
 import {useContext} from "react";
 import {UrlContext} from "@utils/builder.ts";
 import {CircularProgress} from "@mui/material";
+import type {Place} from "@utils/types.ts"
 
 
 export default function ReturnBar() {
     const { url } = useContext(UrlContext)
 
-    const [data, loading] = useFetch(url)
-    let searchLocation;
-
-    if (data != null) {
-        searchLocation = data?.[0]?.display_name;
-    }
+    const [data, loading] = useFetch<Place[]>(url)
 
     return (
         <div className={styles.returnBar}>
                 {
                     loading ?
-                        <div className={styles.returnLoader}> <Loading /></div>   :
-                        searchLocation
+                        <div className={styles.returnLoader}> <Loading /></div> :
+                        data?.slice(0, 5).map(place => (
+                            <div key={place.place_id}>
+                                {place.display_name}</div>
+                        ))
                 }
 
         </div>
