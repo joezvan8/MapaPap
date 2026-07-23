2157b9c34a041c702766c795ddeea1810227871c
oyemiwisdom <oyemiwisdom@gmail.com>
2026-07-23 17:35:14 -0400

    Add documentation for all components.

--- src/components/AccountBtn/AccountBtn.tsx
+++ src/components/AccountBtn/AccountBtn.tsx
@@ -1,6 +1,9 @@
+/* Import styles from css and account icon from Material UI */
+
 import styles from './AccountBtn.module.css';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
+/* Export functionality to App.tsx */
 export default function AccountBtn(){
 
     return (
--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,29 +1,32 @@
+/* Imports */
 import { Box, TextField } from '@mui/material';
 import styles from './SearchBar.module.css';
 import urlBuilder from '@utils/builder.ts';
 import {useContext} from "react";
 import {UrlContext} from "@utils/builder.ts";
 
+/* Constants created to build URL */
 const header = import.meta.env.VITE_BASE_URL;
 const format = 'jsonv2'
-
+/* Search bar function that implements URL building */
 function SearchBar() {
     const { setUrl } = useContext(UrlContext)
 
     const submit = (event: React.SubmitEvent<HTMLFormElement>) => {
         event.preventDefault();
 
-
+        /* Takes user input and parses it into query */
         const userInput = new FormData(event.currentTarget);
         const query = (userInput.get('search') as string || '');
 
+        /* Creates new URL using earlier format and new query */
         const newUrl = urlBuilder(header, query, format);
         setUrl(newUrl);
         console.log(newUrl)
 
     }
-
-      return (
+    /* Creates a box element that implements search/URL functionality */
+    return (
               <Box
                 className={styles.searchBar}
                 component="form"
@@ -44,4 +47,5 @@ function SearchBar() {
   )
 }
 
+/* Export search bar element to App.tsx */
 export default SearchBar;
