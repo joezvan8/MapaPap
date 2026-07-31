257d8807be9319198220293ee96649bb4248df01
Jovan Clue <jovan.clue@bison.howard.edu>
2026-07-31 09:46:27 -0500

    Set up basic backend server using python

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,40 @@
+# imports
 import os
 import requests
+from supabase import create_client
+import json
 
-from flask import Flask
+
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+from flask_cors import CORS
+CORS(app)
+
+
+# routes
+@app.route('/')
+def index():
+    return "This works dw"
+
+
+@app.route('/db/supabase', methods=["POST"])
+def postto_db():
+    supabase = create_client(os.getenv('SUPABASE_URL'), os.getenv('SUPABASE_SECRET_KEY'))  # type: ignore
+
+    response = supabase.table('Search Results').insert({"location": "Rutgers Univ"}).execute()
+    return "Data added: ", jsonify(response.data)
+
+
+@app.route('/db/supabase', methods=["GET"])
+def readfrom_db():
+    supabase = create_client(os.getenv('SUPABASE_URL'), os.getenv('SUPABASE_SECRET_KEY'))  # type: ignore
+
+    response = supabase.table('Search Results').select('*').execute()
+    return jsonify(response.data)
+
+
 
 
 if __name__ == '__main__':
